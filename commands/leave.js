4b9452c1abc86baded03776c3d881605056a2046
Nir <nirnir122@gmail.com>
2018-11-17 14:11:32 +0200

    fixed leave command

--- commands/leave.js
+++ commands/leave.js
@@ -1,13 +1,17 @@
+const playFile = require("./play.js");
 
 module.exports = {
   name: "leave",
   cooldown: 5,
   description: `makes the bot leave the voice channel he's in`,
   execute(message) {
-    if (message.guild.voiceConnection) {
+    if (!message.guild) return;
+    const dispatcher = playFile.dispatcher;
+    if (!message.guild.voiceConnection) {
+      return message.reply("I'm not in a voice channel right now");
+    } else if (message.guild.voiceConnection) {
+      dispatcher.end();
       return message.guild.voiceConnection.disconnect();
-    } else {
-      return message.reply(`I'm not in a channel right now`);
     }
   }
 };
