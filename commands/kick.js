8cd0d769ccbdddfc34d705cce01650261620de2d
Nir <nirnir122@gmail.com>
2018-11-18 10:52:08 +0200

    implemented kick command

--- commands/kick.js
+++ commands/kick.js
@@ -4,20 +4,33 @@ module.exports = {
   cooldown: 5,
   guildOnly: true,
   execute(message) {
-    if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    if (
+      !message.member.hasPermission([
+        "MANAGE_MESSAGES",
+        "KICK_MEMBERS",
+        "BAN_MEMBERS"
+      ])
+    )
       return message.channel.send("No permission!");
-    if (bannedUser.hasPermission("MANAGE_MESSAGES"))
-      return message.channel.send("This person is too important to be banned");
-    message.channel.send("kick command still not implemented");
+    const kicker = message.member;
     if (!message.mentions.users.size) {
       return message.reply("you need to tag a user in order to kick them!");
     }
-
     const taggedUser = message.mentions.users.first();
-
-    member
+    if (message.guild.member(taggedUser).hasPermission("MANAGE_MESSAGES")) {
+      return message.channel.send("This person is too important to be banned");
+    }
+    message.guild
+      .member(taggedUser)
       .kick()
-      .then(() => console.log("Kicked ${member.displayName}"))
-      .catch(console.error);
+      .then(guildMember => {
+        message.channel.send(`${kicker} kicked ${guildMember.user.username}`);
+      })
+      .catch(err => {
+        console.error(err);
+        message.channel.send(
+          "Something went wrong when trying to kick this person"
+        );
+      });
   }
 };
