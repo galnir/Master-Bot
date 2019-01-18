a3e562bbc8bc07177b81f7fd2fe76ea976b2b1ea
Nir <nirnir122@gmail.com>
2019-01-18 19:08:01 +0200

    add error handling for the fortune cookie command

--- commands/fortune.js
+++ commands/fortune.js
@@ -6,12 +6,16 @@ module.exports = {
   cooldown: 10,
   description: "Returnes random fortune cookie",
   async execute(message) {
-    const res = await fetch("http://yerkee.com/api/fortune");
-    const json = await res.json();
-    const embed = new Discord.MessageEmbed()
-      .setColor("RANDOM")
-      .setTitle("Fortune Cookie")
-      .setDescription(json.fortune);
-    message.channel.send(embed);
+    try {
+      const res = await fetch("http://yerkee.com/api/fortune");
+      const json = await res.json();
+      const embed = new Discord.MessageEmbed()
+        .setColor("RANDOM")
+        .setTitle("Fortune Cookie")
+        .setDescription(json.fortune);
+      message.channel.send(embed);
+    } catch (e) {
+      message.channel.send("Failed to open the fortune cookie :confused: ");
+    }
   }
 };
