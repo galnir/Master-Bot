bf22420aa214710aa25d76759253ab1e765fba88
Nir <nirnir122@gmail.com>
2019-01-18 19:45:54 +0200

    add another implementation of fortune command

--- commands/fortune.js
+++ commands/fortune.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const fetch = require("node-fetch");
+// const fs = require("fs");
 
 module.exports = {
   name: "fortune",
@@ -15,7 +16,28 @@ module.exports = {
         .setDescription(json.fortune);
       message.channel.send(embed);
     } catch (e) {
-      message.channel.send("Failed to open the fortune cookie :confused: ");
+      message.channel.send("Could not obtain fortune cookie :confused: ");
     }
+
+    /*
+    The above way is obtaining a fortune cookie through an api,
+    the second way is getting an array of quotes from a file(in resources directory)
+    and pulling a random quote each time the command is called.
+    */
+
+    // try {
+    //   const fortuneArray = fs
+    //     .readFileSync("resources/fortunecookie.txt", "utf8")
+    //     .split("\n");
+    //   const fortuneCookie =
+    //     fortuneArray[Math.floor(Math.random() * fortuneArray.length)];
+    //   const embed = new Discord.MessageEmbed()
+    //     .setColor("RANDOM")
+    //     .setTitle("Fortune Cookie")
+    //     .setDescription(fortuneCookie);
+    //   message.channel.send(embed);
+    // } catch (e) {
+    //   message.channel.send("Could not obtain fortune cookie :confused: ");
+    // }
   }
 };
