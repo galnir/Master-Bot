670f5c14cd7db54f043a7889b0dd99421ee93df5
Nir <nirnir122@gmail.com>
2019-02-23 11:25:30 +0200

    rewrite global-news command using commando

---
+++ commands/other/global-news.js
@@ -0,0 +1,53 @@
+const { RichEmbed } = require("discord.js");
+const fetch = require("node-fetch");
+const { newsAPI } = require("../../config.json");
+const { Command } = require("discord.js-commando");
+
+module.exports = class GlobalNewsCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "global-news",
+      aliases: ["world-news", "reuters"],
+      group: "other",
+      memberName: "global-news",
+      description: "Replies with the 5 latest global news headlines",
+      throttling: {
+        usages: 2,
+        duration: 10
+      }
+    });
+  }
+
+  async run(message) {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?sources=reuters&pageSize=5&apiKey=${newsAPI}`
+      );
+      const json = await response.json();
+      const articleArr = json.articles;
+      let processArticle = article => {
+        const embed = new RichEmbed()
+          .setColor("#FF4F00")
+          .setTitle(article.title)
+          .setURL(article.url)
+          .setAuthor(article.author)
+          .setDescription(article.description)
+          .setThumbnail(article.urlToImage)
+          .setTimestamp(article.publishedAt)
+          .setFooter("---------------------------------");
+        return embed;
+      };
+      async function processArray(array) {
+        for (const article of array) {
+          const msg = await processArticle(article);
+          message.say(msg);
+        }
+      }
+      await processArray(articleArr);
+    } catch (e) {
+      message.say("Something failed along the way");
+      return console.error(e);
+    }
+  }
+};
+// The news api is powered by NewsAPI.org!
