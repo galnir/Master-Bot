c6528cb26341dc3d23d86c91c28a7e50217feb44
Nir <nirnir122@gmail.com>
2019-02-23 16:38:43 +0200

    rewrite ynet-news command using commando

---
+++ commands/other/ynet-news.js
@@ -0,0 +1,52 @@
+const { RichEmbed } = require("discord.js");
+const { newsAPI } = require("../../config.json");
+const { Command } = require("discord.js-commando");
+const fetch = require("node-fetch");
+
+module.exports = class YnetNewsCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: "ynet-news",
+      aliases: ["israel-news", "ynet"],
+      group: "other",
+      memberName: "ynet-news",
+      description: "Replies with the 5 latest israeli news headlines",
+      throttling: {
+        usages: 2,
+        duration: 10
+      }
+    });
+  }
+
+  async run(message) {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?sources=ynet&pageSize=5&apiKey=${newsAPI}`
+      );
+      const json = await response.json();
+      let articleArr = json.articles;
+      let processArticle = article => {
+        let embed = new RichEmbed()
+          .setColor("#BA160C")
+          .setTitle(article.title)
+          .setURL(article.url)
+          .setAuthor(article.author)
+          .setDescription(article.description)
+          .setThumbnail(article.urlToImage)
+          .setTimestamp(article.publishedAt)
+          .setFooter("---------------------------------");
+        return embed;
+      };
+      async function processArray(array) {
+        for (let article of array) {
+          let msg = await processArticle(article);
+          message.say(msg);
+        }
+      }
+      await processArray(articleArr);
+    } catch (err) {
+      message.say("Something failed along the way");
+      return console.error(err);
+    }
+  }
+};
