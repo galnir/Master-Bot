ec423b51dad8fcd750d83901684148324495b22c
Nir <nirnir122@gmail.com>
2019-03-02 16:04:25 +0200

    register guild group

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-const { CommandoClient } = require("discord.js-commando");
-const path = require("path");
-const { prefix, token } = require("./config.json");
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+const { prefix, token } = require('./config.json');
 
 const client = new CommandoClient({
   commandPrefix: prefix,
-  owner: "183647046564184065",
+  owner: '183647046564184065',
   unknownCommandResponse: false
 });
 
 client.registry
   .registerDefaultTypes()
   .registerGroups([
-    ["music", "Music Command Group"],
-    ["gifs", "Gif Command Group"],
-    ["other", "random types of commands group"]
+    ['music', 'Music Command Group'],
+    ['gifs', 'Gif Command Group'],
+    ['other', 'random types of commands group'],
+    ['guild', 'guild related commands']
   ])
   .registerDefaultGroups()
   .registerDefaultCommands()
-  .registerCommandsIn(path.join(__dirname, "commands"));
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
-client.once("ready", () => {
-  console.log("Ready!");
-  client.user.setActivity("!help for commands", "WATCHING");
+client.once('ready', () => {
+  console.log('Ready!');
+  client.user.setActivity('!help for commands', 'WATCHING');
 });
 
 client.login(token);
