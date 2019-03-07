6ecb881e174210bb01390b2d7c34ea1566e67912
Nir <nirnir122@gmail.com>
2019-03-07 17:53:51 +0200

    add missing return statement

--- commands/create-text-channel.js
+++ commands/create-text-channel.js
@@ -1,22 +1,22 @@
 module.exports = {
-  name: "create-text-channel",
+  name: 'create-text-channel',
   cooldown: 5,
-  description: "Create a text channel",
+  description: 'Create a text channel',
   execute(message, args) {
     if (!message.guild) return;
     // check if user is an admin
-    if (!message.member.hasPermission("MANAGE_MESSAGES"))
-      return message.reply("You have no permission to create channels!");
+    if (!message.member.hasPermission('MANAGE_MESSAGES'))
+      return message.reply('You have no permission to create channels!');
     // check if provided args
-    if (!args) return message.reply("Please provide a channel name!");
+    if (!args) return message.reply('Please provide a channel name!');
     // stringify the args
     const channelName = args.toString();
     // no more than 20 characters
     if (channelName.length > 20)
-      return message.reply("Channel names need to be less than 20 characters");
+      return message.reply('Channel names need to be less than 20 characters');
     message.guild.channels
       .create(channelName, {
-        type: "text"
+        type: 'text'
       })
       .then(
         message.channel.send(
@@ -24,10 +24,10 @@ module.exports = {
         )
       )
       .catch(e => {
-        message.channel.send(
-          "An error has occured when trying to create a text channel"
-        );
         console.log(e);
+        return message.channel.send(
+          'An error has occured when trying to create a text channel'
+        );
       });
   }
 };
