16dc8fc569fcd6964b39f6ba9c2312a8dbc0cf62
Nir <nirnir122@gmail.com>
2019-03-08 17:19:29 +0200

    remove unnecessary async await in favor of catch method

--- commands/translate.js
+++ commands/translate.js
@@ -1,64 +1,63 @@
-const Discord = require("discord.js");
-const fetch = require("node-fetch");
-const { yandexAPI } = require("../config.json");
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const { yandexAPI } = require('../config.json');
 
 module.exports = {
-  name: "translate",
+  name: 'translate',
   cooldown: 5,
   description:
-    "Translate to English(for now) from any supported language using yandex",
-  async execute(message, args) {
+    'Translate to English(for now) from any supported language using yandex',
+  execute(message, args) {
     if (!args)
       return message.reply(
-        "Please provide something to translate and to what language"
+        'Please provide something to translate and to what language'
       );
-    const text = args.join(" ");
+    const text = args.join(' ');
     let lang;
     // I'm using encodeURI() because of a TypeError that is happening with some languages
-    try {
-      await fetch(
-        // Powered by Yandex.Translate http://translate.yandex.com/
-        `https://translate.yandex.net/api/v1.5/tr.json/detect?key=${yandexAPI}&text=${encodeURI(
-          text
-        )}`
-      )
-        .then(res => res.json())
-        .then(json => {
-          lang = json.lang;
-        });
-    } catch (e) {
-      console.error(e);
-      return message.channel.send(
-        "Something went wrong when trying to detect language"
-      );
-    }
-    if (!lang) return message.reply("Provide characters only!");
+    fetch(
+      // Powered by Yandex.Translate http://translate.yandex.com/
+      `https://translate.yandex.net/api/v1.5/tr.json/detect?key=${yandexAPI}&text=${encodeURI(
+        text
+      )}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        lang = json.lang;
+      })
+      .catch(e => {
+        console.error(e);
+        return message.channel.send(
+          'Something went wrong when trying to detect language'
+        );
+      });
 
-    try {
-      await fetch(
-        // Powered by Yandex.Translate http://translate.yandex.com/
-        `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${yandexAPI}&text=${encodeURI(
-          text
-        )}&lang=${lang + "-" + "en"}`
-      )
-        .then(res => res.json())
-        .then(json => {
-          return message.channel.send(embedTranslation(json.text[0]));
-        });
-    } catch (e) {
-      console.error(e);
-      return message.channel.send(
-        "Something went wrong when trying to translate"
-      );
-    }
+    if (!lang) return message.reply('Provide characters only!');
+
+    fetch(
+      // Powered by Yandex.Translate http://translate.yandex.com/
+      `https://translate.yandex.net/api/v1.5/tr.json/translate?key=${yandexAPI}&text=${encodeURI(
+        text
+      )}&lang=${lang + '-' + 'en'}`
+    )
+      .then(res => res.json())
+      .then(json => {
+        return message.channel.send(embedTranslation(json.text[0]));
+      })
+      .catch(e => {
+        console.error(e);
+        return message.channel.send(
+          'Something went wrong when trying to translate'
+        );
+      });
 
     function embedTranslation(text) {
       return new Discord.MessageEmbed()
-        .setColor("#FF0000")
-        .setTitle("Yandex Translate")
-        .setURL("http://translate.yandex.com/")
+        .setColor('#FF0000')
+        .setTitle('Yandex Translate')
+        .setURL('http://translate.yandex.com/')
         .setDescription(text)
-        .setFooter("Powered by Yandex.Translate");
+        .setFooter('Powered by Yandex.Translate');
     }
   }
 };
