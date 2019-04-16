0d709cb5e483ee685c8bbfa8720dd87031b778f6
Nir <nirnir122@gmail.com>
2019-04-16 18:17:01 +0300

    replace deprecated RichEmbed with MessageEmbed in fortune.js

--- commands/other/fortune.js
+++ commands/other/fortune.js
@@ -1,16 +1,15 @@
-const fetch = require("node-fetch");
-const { Command } = require("discord.js-commando");
-const { RichEmbed } = require("discord.js");
-// const fs = require("fs");
+const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = class FortuneCommand extends Command {
   constructor(client) {
     super(client, {
-      name: "fortune",
-      aliases: ["fortune-cookie"],
-      group: "other",
-      memberName: "fortune",
-      description: "Replies with a fortune cookie tip",
+      name: 'fortune',
+      aliases: ['fortune-cookie'],
+      group: 'other',
+      memberName: 'fortune',
+      description: 'Replies with a fortune cookie tip',
       throttling: {
         usages: 2,
         duration: 10
@@ -20,15 +19,15 @@ module.exports = class FortuneCommand extends Command {
 
   async run(message) {
     try {
-      const res = await fetch("http://yerkee.com/api/fortune");
+      const res = await fetch('http://yerkee.com/api/fortune');
       const json = await res.json();
-      const embed = new RichEmbed()
-        .setColor("RANDOM")
-        .setTitle("Fortune Cookie")
+      const embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Fortune Cookie')
         .setDescription(json.fortune);
       return message.say(embed);
     } catch (e) {
-      message.say("Could not obtain fortune cookie :confused: ");
+      message.say('Could not obtain fortune cookie :confused: ');
       return console.error(e);
     }
   }
