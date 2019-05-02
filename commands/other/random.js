d311565e3eb3977eaacd7209cabe4d04d3e4f37c
Nir <nirnir122@gmail.com>
2019-05-02 20:05:39 +0300

    write random number generator command

---
+++ commands/other/random.js
@@ -0,0 +1,36 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class RandomNumberCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'random',
+      aliases: ['random-number', 'number-between'],
+      memberName: 'random',
+      group: 'other',
+      description: 'Generate a random number between two provided numbers',
+      args: [
+        {
+          key: 'min',
+          prompt: 'What is the minimum number?',
+          type: 'integer'
+        },
+        {
+          key: 'max',
+          prompt: 'What is the maximum number?',
+          type: 'integer'
+        }
+      ]
+    });
+  }
+
+  run(message, { min, max }) {
+    try {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return message.say(Math.floor(Math.random() * (max - min + 1)) + min);
+    } catch (err) {
+      console.error(err);
+      return message.say('Failed to generate a random number');
+    }
+  }
+};
