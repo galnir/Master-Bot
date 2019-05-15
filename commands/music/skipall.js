b8cdeca01398f059b4a012c42a713a36a31d0e41
Nir <nirnir122@gmail.com>
2019-05-15 20:41:57 +0300

    add a skipall command which skips all songs in queue

---
+++ commands/music/skipall.js
@@ -0,0 +1,29 @@
+const { Command } = require('discord.js-commando');
+const playFile = require('./play.js');
+
+module.exports = class SkipAllCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'skipall',
+      aliases: ['skip-all'],
+      memberName: 'skipall',
+      group: 'music',
+      description: 'Skip all songs in queue',
+      guildOnly: true
+    });
+  }
+
+  run(message) {
+    var voiceChannel = message.member.voice.channel;
+    if (!voiceChannel) return message.reply('Join a channel and try again');
+
+    var dispatcher = playFile.dispatcher;
+
+    if (typeof dispatcher == 'undefined') {
+      return message.reply('There is no song playing right now!');
+    }
+    if (!playFile.queue) return message.say('There are no songs in queue');
+    dispatcher.end();
+    return (playFile.queue.length = 0); // clear queue
+  }
+};
