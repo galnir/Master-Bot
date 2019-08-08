e4ddb2f5592ca00a19ffc896ddd06219f13ffbc4
Nir <nirnir122@gmail.com>
2019-08-08 18:45:27 +0300

    remove async await from a chained promise

--- commands/other/cat.js
+++ commands/other/cat.js
@@ -1,15 +1,15 @@
-const fetch = require("node-fetch");
-const { tenorAPI } = require("../../config.json");
-const { Command } = require("discord.js-commando");
+const fetch = require('node-fetch');
+const { tenorAPI } = require('../../config.json');
+const { Command } = require('discord.js-commando');
 
 module.exports = class CatCommand extends Command {
   constructor(client) {
     super(client, {
-      name: "cat",
-      aliases: ["cat-pic", "cats"],
-      group: "other",
-      memberName: "cat",
-      description: "Replies with a cute cat picture",
+      name: 'cat',
+      aliases: ['cat-pic', 'cats'],
+      group: 'other',
+      memberName: 'cat',
+      description: 'Replies with a cute cat picture',
       throttling: {
         usages: 2,
         duration: 10
@@ -17,16 +17,13 @@ module.exports = class CatCommand extends Command {
     });
   }
 
-  async run(message) {
-    try {
-      await fetch(
-        `https://api.tenor.com/v1/random?key=${tenorAPI}&q=cat&limit=1`
-      )
-        .then(res => res.json())
-        .then(json => message.say(json.results[0].url));
-    } catch (err) {
-      message.say("Request to find a kitty failed :(");
-      return console.error(err);
-    }
+  run(message) {
+    fetch(`https://api.tenor.com/v1/random?key=${tenorAPI}&q=cat&limit=1`)
+      .then(res => res.json())
+      .then(json => message.say(json.results[0].url))
+      .catch(e => {
+        message.say('Request to find a kitty failed :(');
+        return console.error(e);
+      });
   }
 };
