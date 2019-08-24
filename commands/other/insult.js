911e8c6cddd42afbc069e8383b38b63ec25577ad
Nir <nirnir122@gmail.com>
2019-08-24 20:22:39 +0300

    add insult command

---
+++ commands/other/insult.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class NBACommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'insult',
+      group: 'other',
+      memberName: 'insult',
+      description: 'Generate an evil insult!',
+      throttling: {
+        usages: 1,
+        duration: 6
+      }
+    });
+  }
+
+  run(message) {
+    // thanks to https://evilinsult.com :)
+    fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json')
+      .then(res => res.json())
+      .then(json => {
+        const embed = new MessageEmbed()
+          .setColor('#E41032')
+          .setTitle('Evil Insult')
+          .setDescription(json.insult)
+          .setURL('https://evilinsult.com');
+        return message.say(embed);
+      })
+      .catch(e => {
+        message.say('Failed to deliver insult :sob:');
+        return console.error(e);
+      });
+  }
+};
