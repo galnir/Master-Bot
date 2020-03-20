0d560720bbd11a9d6e9a91eb29b3f5a34fcca516
Nir <nirnir122@gmail.com>
2020-03-20 22:59:09 +0200

    update channel greeting to Managers

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.once('ready', () => {
 });
 
 client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find(c => c.name === 'general'); // change this to the channel name you want to send the greeting to
+  const channel = member.guild.channels.cache.find(c => c.name === 'general'); // change this to the channel name you want to send the greeting to
   if (!channel) return;
   channel.send(`Welcome ${member}!`);
 });
