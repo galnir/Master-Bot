344b15afa2c1e6cb1eae95b20461144d75952569
Nir <36937694+galnir@users.noreply.github.com>
2020-05-11 12:57:30 +0300

    Update ban.js

--- commands/guild/ban.js
+++ commands/guild/ban.js
@@ -45,7 +45,7 @@ module.exports = class BanCommand extends Command {
       })
       .catch(e => {
         message.say(
-          'Something went wrong when trying to kick this user, I probably do not have the permission to ban him'
+          'Something went wrong when trying to ban this user, I probably do not have the permission to ban him'
         );
         return console.error(e);
       });
