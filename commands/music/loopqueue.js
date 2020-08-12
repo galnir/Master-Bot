21fdd573bce06ab1ade040dcf6b04ea437085cc0
Thaddeus Kuah <thaddeuskkr@gmail.com>
2020-08-12 21:14:16 +0800

    Update loopqueue.js

--- commands/music/loopqueue.js
+++ commands/music/loopqueue.js
@@ -1,7 +1,7 @@
 const { Command } = require('discord.js-commando');
 //const { MessageEmbed } = require('discord.js');
 
-module.exports = class TestCommand extends Command {
+module.exports = class LoopQueueCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'loopqueue',
