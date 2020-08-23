b5c7f645ec014b70d2ba9ef16a436a313837bd8d
Nir <nirnir122@gmail.com>
2020-08-23 19:12:30 +0300

    rewrite lyrics command

--- commands/music/lyrics.js
+++ commands/music/lyrics.js
@@ -9,12 +9,13 @@ module.exports = class LyricsCommand extends Command {
     super(client, {
       name: 'lyrics',
       memberName: 'lyrics',
+      aliases: ['lr'],
       description:
         'Get lyrics of any song or the lyrics of the currently playing song',
       group: 'music',
       throttling: {
         usages: 1,
-        duration: 5
+        duration: 10
       },
       args: [
         {
@@ -43,70 +44,136 @@ module.exports = class LyricsCommand extends Command {
     const sentMessage = await message.channel.send(
       '👀 Searching for lyrics 👀'
     );
-    // get song id
-    songName = songName.replace(/ *\([^)]*\) */g, ''); // remove stuff like (Official Video)
+
+    // remove stuff like (Official Video)
+    songName = songName.replace(/ *\([^)]*\) */g, '');
+
+    // remove emojis
     songName = songName.replace(
       /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
       ''
-    ); // remove emojis
-    var url = `https://api.genius.com/search?q=${encodeURI(songName)}`;
+    );
 
-    const headers = {
-      Authorization: `Bearer ${geniusLyricsAPI}`
-    };
-    try {
-      var body = await fetch(url, { headers });
-      var result = await body.json();
-      const songPath = result.response.hits[0].result.api_path;
-      // get lyrics
-      url = `https://api.genius.com${songPath}`;
-      body = await fetch(url, { headers });
-      result = await body.json();
-
-      const song = result.response.song;
-
-      let lyrics = await getLyrics(song.url);
-      lyrics = lyrics.replace(/(\[.+\])/g, '');
-      if (!lyrics) {
-        return message.say(
-          'No lyrics have been found for your query, please try again and be more specific.'
-        );
-      }
-
-      if (lyrics.length > 4095)
-        return message.say(
-          'Lyrics are too long to be returned in a message embed'
-        );
-      if (lyrics.length < 2048) {
-        const lyricsEmbed = new MessageEmbed()
-          .setColor('#00724E')
-          .setDescription(lyrics.trim());
-        return sentMessage.edit('', lyricsEmbed);
-      } else {
-        // 2048 < lyrics.length < 4096
-        const firstLyricsEmbed = new MessageEmbed()
-          .setColor('#00724E')
-          .setDescription(lyrics.slice(0, 2048));
-        const secondLyricsEmbed = new MessageEmbed()
-          .setColor('#00724E')
-          .setDescription(lyrics.slice(2048, lyrics.length));
-        sentMessage.edit('', firstLyricsEmbed);
-        message.channel.send(secondLyricsEmbed);
+    LyricsCommand.searchSong(songName)
+      .then(function(url) {
+        LyricsCommand.getSongPageURL(url)
+          .then(function(url) {
+            LyricsCommand.getLyrics(url)
+              .then(function(lyrics) {
+                if (lyrics.length > 4095) {
+                  message.say(
+                    'Lyrics are too long to be returned in a message embed'
+                  );
+                  return;
+                }
+                if (lyrics.length < 2048) {
+                  const lyricsEmbed = new MessageEmbed()
+                    .setColor('#00724E')
+                    .setDescription(lyrics.trim());
+                  return sentMessage.edit('', lyricsEmbed);
+                } else {
+                  // 2048 < lyrics.length < 4096
+                  const firstLyricsEmbed = new MessageEmbed()
+                    .setColor('#00724E')
+                    .setDescription(lyrics.slice(0, 2048));
+                  const secondLyricsEmbed = new MessageEmbed()
+                    .setColor('#00724E')
+                    .setDescription(lyrics.slice(2048, lyrics.length));
+                  sentMessage.edit('', firstLyricsEmbed);
+                  message.channel.send(secondLyricsEmbed);
+                  return;
+                }
+              })
+              .catch(function(err) {
+                message.say(err);
+                return;
+              });
+          })
+          .catch(function(err) {
+            message.say(err);
+            return;
+          });
+      })
+      .catch(function(err) {
+        message.say(err);
         return;
+      });
+  }
+
+  static searchSong(query) {
+    return new Promise(async function(resolve, reject) {
+      const searchURL = `https://api.genius.com/search?q=${encodeURI(query)}`;
+      const headers = {
+        Authorization: `Bearer ${geniusLyricsAPI}`
+      };
+      try {
+        const body = await fetch(searchURL, { headers });
+        const result = await body.json();
+        const songPath = result.response.hits[0].result.api_path;
+        resolve(`https://api.genius.com${songPath}`);
+      } catch (e) {
+        reject('No song has been found for this query');
       }
-    } catch (e) {
-      sentMessage.edit(
-        'Something when wrong, please try again or be more specific'
-      );
-      return;
-    }
-    async function getLyrics(url) {
-      const response = await fetch(url);
-      const text = await response.text();
-      const $ = cheerio.load(text);
-      return $('.lyrics')
-        .text()
-        .trim();
-    }
+    });
+  }
+
+  static getSongPageURL(url) {
+    return new Promise(async function(resolve, reject) {
+      const headers = {
+        Authorization: `Bearer ${geniusLyricsAPI}`
+      };
+      try {
+        const body = await fetch(url, { headers });
+        const result = await body.json();
+        if (!result.response.song.url) {
+          reject('There was a problem finding a URL for this song');
+        } else {
+          resolve(result.response.song.url);
+        }
+      } catch (e) {
+        reject('There was a problem finding a URL for this song');
+      }
+    });
+  }
+
+  static getLyrics(url) {
+    return new Promise(async function(resolve, reject) {
+      try {
+        const response = await fetch(url);
+        const text = await response.text();
+        const $ = cheerio.load(text);
+        const lyrics = $('.lyrics')
+          .text()
+          .trim();
+        if (!lyrics) {
+          try {
+            // try again, Genius sometimes changes the div class names
+            const response = await fetch(url);
+            const text = await response.text();
+            const $ = cheerio.load(text);
+            const lyrics = $('.lyrics')
+              .text()
+              .trim();
+            if (!lyrics) {
+              reject(
+                'There was a problem fetching lyrics for this song, please try again'
+              );
+            } else {
+              resolve(lyrics.replace(/(\[.+\])/g, ''));
+            }
+          } catch (e) {
+            reject(
+              'There was a problem fetching lyrics for this song, please try again'
+            );
+          }
+        } else {
+          resolve(lyrics.replace(/(\[.+\])/g, ''));
+        }
+      } catch (e) {
+        reject(
+          'There was a problem fetching lyrics for this song, please try again'
+        );
+      }
+    });
   }
 };
