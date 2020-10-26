8d143353ac652ee87f8190bf11b715291823e3ca
Charlie <13785654+ModoSN@users.noreply.github.com>
2020-10-26 13:58:10 +0000

    Create rockpaperscissors.js

---
+++ commands/other/rockpaperscissors.js
@@ -0,0 +1,35 @@
+const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class RPSCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'rps',
+      aliases: ['rock'],
+      group: 'other',
+      memberName: 'other',
+      description: 'Rock paper scissors',
+      throttling: {
+        usages: 2,
+        duration: 10
+      }
+    });
+  }
+
+  async run(message) {
+    try {
+      const replies = ["Rock", "Paper", "Scissors"];
+	  const reply = replies[Math.floor(Math.random() * replies.length)];
+	  
+      const embed = new MessageEmbed()
+        .setColor('RANDOM')
+        .setTitle('Rock, Paper, Scissors')
+        .setDescription(`**${reply}**`);
+      return message.say(embed);
+    } catch (e) {
+      message.say('Could not play :confused: ');
+      return console.error(e);
+    }
+  }
+};
