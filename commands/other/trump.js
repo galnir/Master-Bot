61bd9cca4bc7fe75d8b8832ddc1d1e0ebda27a26
Charlie <13785654+ModoSN@users.noreply.github.com>
2020-10-28 15:51:01 +0000

    Create trump.js
    Get random Donald Trump quotes

---
+++ commands/other/trump.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class TrumpCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'trump',
+      group: 'other',
+      memberName: 'trump',
+      description: 'Get a random quote from Donald Trump!',
+      throttling: {
+        usages: 1,
+        duration: 6
+      }
+    });
+  }
+
+  run(message) {
+    fetch('https://api.tronalddump.io/random/quote')
+      .then(res => res.json())
+      .then(json => {
+        const embed = new MessageEmbed()
+          .setColor('#BB7D61')
+          .setTitle('Quotes from Trump')
+          .setDescription(json.value)
+		  .setTimestamp()
+		  .setFooter('Powered tronalddump.io', 'https://www.whitehouse.gov/wp-content/uploads/2017/11/President-Trump-Official-Portrait-200x200.jpg');
+        return message.say(embed);
+      })
+      .catch(err => {
+        message.say('Failed to deliver quote :sob:');
+        return console.error(err);
+      });
+  }
+};
