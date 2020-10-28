9e7cb2d24eb73e4eefd35016c4b040e9ea892fe5
Charlie <13785654+ModoSN@users.noreply.github.com>
2020-10-28 16:07:35 +0000

    Create advice.js
    Random advice from adviceslip.com

---
+++ commands/other/advice.js
@@ -0,0 +1,36 @@
+const fetch = require('node-fetch');
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class AdviceCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'advice',
+      group: 'other',
+      memberName: 'advice',
+      description: 'Get some advice!',
+      throttling: {
+        usages: 1,
+        duration: 6
+      }
+    });
+  }
+
+  run(message) {
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(json => {
+        const embed = new MessageEmbed()
+          .setColor('#403B3A')
+          .setTitle('Advice Slip')
+          .setDescription(json.slip.advice)
+		  .setTimestamp()
+		  .setFooter('Powered by adviceslip.com', 'https://i.imgur.com/8pIvnmD.png');
+        return message.say(embed);
+      })
+      .catch(err => {
+        message.say('Failed to deliver advice :sob:');
+        return console.error(err);
+      });
+  }
+};
