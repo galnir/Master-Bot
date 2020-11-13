79418627185319478c309e1c927504ff1152a2e8
Nir <nirnir122@gmail.com>
2020-11-13 09:11:29 +0200

    fix mixed spaces and tabs

--- commands/other/motivation.js
+++ commands/other/motivation.js
@@ -25,10 +25,14 @@ module.exports = class MotivationCommand extends Command {
       quoteArray[Math.floor(Math.random() * quoteArray.length)];
 
     const quoteEmbed = new MessageEmbed()
-      .setAuthor('Motivational Quote', 'https://i.imgur.com/Cnr6cQb.png', 'https://type.fit')
+      .setAuthor(
+        'Motivational Quote',
+        'https://i.imgur.com/Cnr6cQb.png',
+        'https://type.fit'
+      )
       .setDescription(`*"${randomQuote.text}*"\n\n-${randomQuote.author}`)
-	  .setTimestamp()
-	  .setFooter('Powered by type.fit')
+      .setTimestamp()
+      .setFooter('Powered by type.fit')
       .setColor('#FFD77A');
     return message.channel.send(quoteEmbed);
   }
