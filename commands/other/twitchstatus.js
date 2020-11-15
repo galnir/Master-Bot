7f4baf7f4ff8a4c6be62338548515ebe059040bb
Bacon-Fixation <baconfixation@gmail.com>
2020-11-15 02:00:33 -0600

    cleanup

--- commands/other/twitchstatus.js
+++ commands/other/twitchstatus.js
@@ -31,19 +31,21 @@ module.exports = class TwitchStatusCommand extends Command {
       const access_token = result.access_token;
 
       const user = await Twitch.getUserInfo(access_token, twitchClientID, text);
+
       if (user.data[0] == null)
-      return message.reply(`:x: No streamer found called ${text}`);
-        let user_id = user.data[0].id;
+        return message.reply(`:x: No streamer found called ${text}`);
+
+      const user_id = user.data[0].id;
+
       const streamInfo = await Twitch.getStream(
         access_token,
         twitchClientID,
         user_id
       );
-        //console.log(streamInfo.data[0])
-      if (streamInfo.data[0] == undefined)
-        return message.reply(text + ' is not currently streaming');
+      if (streamInfo.data[0] == null)
+        return message.say(text + ' is not currently streaming');
 
-        const gameInfo = await Twitch.getGames(
+      const gameInfo = await Twitch.getGames(
         access_token,
         twitchClientID,
         streamInfo.data[0].game_id
@@ -51,10 +53,11 @@ module.exports = class TwitchStatusCommand extends Command {
 
       const onlineEmbed = new MessageEmbed()
         .setAuthor(
-          text + ' is online!',
+          'Streamer Status Check',
           user.data[0].profile_image_url,
           `https://twitch.tv/${streamInfo.data[0].user_name}`
         )
+        .setTitle('Looks like ' + text + ' is online!')
         .setThumbnail(
           gameInfo.data[0].box_art_url.replace(/-{width}x{height}/g, '')
         )
@@ -62,11 +65,21 @@ module.exports = class TwitchStatusCommand extends Command {
         .addField('Currently Playing:', streamInfo.data[0].game_name, true)
         .addField('Viewers:', streamInfo.data[0].viewer_count, true)
         .setColor('#6441A4')
-        .setFooter('Stream Started:')
+        .setFooter('Stream Started')
         .setImage(
           streamInfo.data[0].thumbnail_url.replace(/-{width}x{height}/g, '')
         )
         .setTimestamp(streamInfo.data[0].started_at);
+      if (
+        (user.data[0].broadcaster_type === 'partner',
+        user.data[0].broadcaster_type === 'affiliate')
+      )
+        onlineEmbed.addField(
+          'Rank:',
+          user.data[0].broadcaster_type.toUpperCase(),
+          true
+        );
+
       message.say(onlineEmbed);
     });
   }
