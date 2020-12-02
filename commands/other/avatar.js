69cfcf7c028db5c7cd78d87cc2de9857d33eec04
Nathaniel <64607128+Natemo6348@users.noreply.github.com>
2020-12-02 14:15:28 -0600

    Create avatar.js
    As requested by @kizule #341 
    May still need testing just in case
    Working on repl.it

---
+++ commands/other/avatar.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class AvatarCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'avatar',
+			aliases: ['profile-picture', 'profile-pic', 'pfp', 'av'],
+			group: 'other',
+			memberName: 'avatar',
+			description: 'Responds with a user\'s avatar.',
+			args: [
+				{
+					key: 'user',
+					prompt: 'Which user would you like to get the avatar of?',
+					type: 'user',
+					default: msg => msg.author
+				}
+			]
+		});
+	}
+
+	run(msg, { user }) {
+		const embed = new MessageEmbed()
+			.setTitle(user.tag)
+			.setImage(user.displayAvatarURL())
+			.setColor(0x00AE86);
+		return msg.embed(embed);
+	}
+};
