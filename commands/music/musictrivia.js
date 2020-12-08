3b8e6923a4c77aae4d5ca0c4ae521e45a6d74da7
Bacon-Fixation <baconfixation@gmail.com>
2020-12-08 09:26:36 -0600

    error catch

--- commands/music/musictrivia.js
+++ commands/music/musictrivia.js
@@ -105,9 +105,19 @@ module.exports = class MusicTriviaCommand extends Command {
 
           const filter = msg =>
             message.guild.triviaData.triviaScore.has(msg.author.username);
-          const collector = message.channel.createMessageCollector(filter, {
-            time: 30000
-          });
+          const collector = message.channel
+            .createMessageCollector(filter, {
+              time: 30000
+            })
+            .on('error', function(e) {
+              message.say(':x: Music Trivia has stopped could not play song!');
+              console.log(e);
+              message.guild.musicData.isPlaying = false;
+              message.guild.triviaData.isTriviaRunning = false;
+              message.guild.triviaData.triviaScore.clear();
+              message.guild.musicData.songDispatcher = null;
+              message.guild.me.voice.channel.leave();
+            });
 
           collector.on('collect', msg => {
             if (!message.guild.triviaData.triviaScore.has(msg.author.username))
