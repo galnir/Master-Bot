b4057a2db7c636c3102ccc416fe45625d3d45a45
Nir <36937694+galnir@users.noreply.github.com>
2020-12-11 12:51:53 +0200

    change arrow function to explicit function

--- commands/other/avatar.js
+++ commands/other/avatar.js
@@ -2,29 +2,31 @@ const { Command } = require('discord.js-commando');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = class AvatarCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'avatar',
-			aliases: ['profile-picture', 'profile-pic', 'pfp', 'av'],
-			group: 'other',
-			memberName: 'avatar',
-			description: 'Responds with a user\'s avatar.',
-			args: [
-				{
-					key: 'user',
-					prompt: 'Which user would you like to get the avatar of?',
-					type: 'user',
-					default: msg => msg.author
-				}
-			]
-		});
-	}
+  constructor(client) {
+    super(client, {
+      name: 'avatar',
+      aliases: ['profile-picture', 'profile-pic', 'pfp', 'av'],
+      group: 'other',
+      memberName: 'avatar',
+      description: "Responds with a user's avatar.",
+      args: [
+        {
+          key: 'user',
+          prompt: 'Which user would you like to get the avatar of?',
+          type: 'user',
+          default: function defaultMember(msg) {
+            return msg.author;
+          }
+        }
+      ]
+    });
+  }
 
-	run(msg, { user }) {
-		const embed = new MessageEmbed()
-			.setTitle(user.tag)
-			.setImage(user.displayAvatarURL({ dynamic: true }))
-			.setColor(0x00AE86);
-		return msg.embed(embed);
-	}
+  run(msg, { user }) {
+    const embed = new MessageEmbed()
+      .setTitle(user.tag)
+      .setImage(user.displayAvatarURL({ dynamic: true }))
+      .setColor(0x00ae86);
+    return msg.embed(embed);
+  }
 };
