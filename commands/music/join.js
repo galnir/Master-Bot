297ed614daf334e6aa725688c0dbb943d1a9ddd5
Nir <36937694+galnir@users.noreply.github.com>
2020-12-12 19:27:57 +0200

    Update join.js

--- commands/music/join.js
+++ commands/music/join.js
@@ -15,24 +15,24 @@ module.exports = class JoinCommand extends Command {
     });
   }
 
-  run(message) {
+  async run(message) {
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) {
-      message.say(':no_entry: Please join a voice channel and try again!');
+      message.reply(':no_entry: Please join a voice channel and try again!');
       return;
     }
     if (message.guild.triviaData.isTriviaRunning == true) {
-      message.say(':x: Please try after the trivia has ended!');
+      message.reply(':x: Please try after the trivia has ended!');
       return;
     }
     if (message.guild.musicData.isPlaying != true) {
-      message.say(':x: Nothing is Playing');
+      message.reply(':x: Nothing is Playing');
       return;
     }
     try {
-      voiceChannel.join();
+      await voiceChannel.join();
     } catch {
-      return message.say(':x Something went wrong when moving channels');
+      return message.reply(':x Something went wrong when moving channels');
     }
   }
 };
