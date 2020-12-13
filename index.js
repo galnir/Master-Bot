ec71be5045a1a2c82430041e9b127be0ea299412
Bacon-Fixation <baconfixation@gmail.com>
2020-12-12 18:17:02 -0600

    Update index.js

--- index.js
+++ index.js
@@ -211,12 +211,12 @@ client.on('guildMemberAdd', async member => {
         `:speech_balloon: Hey ${member.displayName}, You look new to ${member.guild.name}!` //<-- didn't play nice being stored in DB
       );
     } else embed.setTitle(welcomeMsgSettings.embedTitle);
-    try {
-      if (welcomeMsgSettings.destination == 'direct message')
+    if (welcomeMsgSettings.destination == 'direct message')
+      try {
         await member.user.send(embed);
-    } catch {
-      console.log(`${member.user.username}'s dms are private`);
-    }
+      } catch {
+        console.log(`${member.user.username}'s dms are private`);
+      }
     if (welcomeMsgSettings.destination != 'direct message') {
       const channel = member.guild.channels.cache.find(
         channel => channel.name === welcomeMsgSettings.destination
