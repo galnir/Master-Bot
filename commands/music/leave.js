424bc9110b74cb2d5cff576f514f016d5ec7de12
Nir <nirnir122@gmail.com>
2021-02-09 18:40:29 +0200

    address #438

--- commands/music/leave.js
+++ commands/music/leave.js
@@ -46,6 +46,7 @@ module.exports = class LeaveCommand extends Command {
       message.guild.musicData.songDispatcher.resume();
       message.guild.musicData.queue.length = 0;
       message.guild.musicData.loopSong = false;
+      message.guild.musicData.skipTimer = true;
       setTimeout(() => {
         message.guild.musicData.songDispatcher.end();
       }, 100);
