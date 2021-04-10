1fd09571b684a374e50f6dcb2017e714e1404be2
Nir <nirnir122@gmail.com>
2021-04-10 14:12:18 +0300

    change permissions required to run and use add-role command

--- commands/guild/add-role.js
+++ commands/guild/add-role.js
@@ -5,12 +5,13 @@ module.exports = class ARCommand extends Command {
   constructor(client) {
     super(client, {
       name: 'ar',
-      aliases: ['assign-role', 'ar'],
+      aliases: ['assign-role', 'ar', 'add-role'],
       memberName: 'assign-role',
       group: 'guild',
+      clientPermissions: ['MANAGE_ROLES'],
+      userPermissions: ['MANAGE_ROLES'],
       description: 'Adds a specific role to a specified user.',
       guildOnly: true,
-      ownerOnly: true,
       args: [
         {
           key: 'userToAssignRole',
@@ -22,36 +23,40 @@ module.exports = class ARCommand extends Command {
           prompt: 'Which role do you want to assign?',
           type: 'string'
         }
-        ]
+      ]
     });
   }
-  
-  async run( message, { userToAssignRole, roleToAssign }) {
-    const extractNumber = /\d+/g;
-    const getuserid = userToAssignRole.match(extractNumber)[0];
-    const user = 
-          message.mentions.members.first() || 
-          (await message.guild.members.fetch(getuserid));
-    const role = message.mentions.roles.first() || 
-          (await message.guild.roles.fetch(roleToAssign));
-    if (user == undefined)
-      return message.channel.send(':x: Please try again with a valid user.');
-    if (role == undefined)
-      return message.channel.send(':x: Please try again with a valid role.');
-    
+
+  async run(message, { userToAssignRole, roleToAssign }) {
+    const getuserid = userToAssignRole.match(/\d+/g)[0];
+    const user =
+      message.mentions.members.first() ||
+      (await message.guild.members.fetch(getuserid));
+    const role =
+      message.mentions.roles.first() ||
+      (await message.guild.roles.fetch(roleToAssign));
+    if (user == undefined) {
+      message.channel.send(':x: Please try again with a valid user.');
+      return;
+    }
+    if (role == undefined) {
+      message.channel.send(':x: Please try again with a valid role.');
+      return;
+    }
+
     user.roles
       .add(role)
       .then(() => {
-        const aroleEmbed = new MessageEmbed()
+        const roleEmbed = new MessageEmbed()
           .addField('Assigned Role', roleToAssign)
           .addField('To', userToAssignRole)
           .setColor(role.hexColor);
-        message.channel.send(aroleEmbed);
+        message.channel.send(roleEmbed);
       })
-      .then( () => message.delete().catch(e => console.error(e)) ) // nested promise
+      .then(() => message.delete().catch(e => console.error(e))) // nested promise
       .catch(err => {
         message.reply(
-          ':x: Something went wrong when trying to assign role to this user, I probably do not have the permission to assign role to him!'
+          ':x: Something went wrong when trying to assign role to this user'
         );
         return console.error(err);
       });
