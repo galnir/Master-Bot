5b9a0192820519b1fbcc5cb54a2313a73417860f
Nimbi <Darkwater409@gmail.com>
2021-04-10 02:34:56 -0700

    Update animegif.js
    Changelog:
     - Placed the image into a RichEmbed to make it look a little bit cleaner
     - Updated the error log

--- commands/gifs/animegif.js
+++ commands/gifs/animegif.js
@@ -1,33 +1,48 @@
 const fetch = require('node-fetch');
-const { tenorAPI } = require('../../config.json');
+const config = require('../../config.json');
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
-// Skips loading if not found in config.json
-if (!tenorAPI) return;
+if (!config.tenorAPI) return;
 
 module.exports = class AnimegifCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'animegif',
-      group: 'gifs',
-      aliases: ['anime-gif', 'anime-gifs'],
-      memberName: 'animegif',
-      description:
-        'Provide a name of an anime show or character and I will return a gif!',
-      throttling: {
-        usages: 1,
-        duration: 4
-      }
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'animegif',
+            group: 'gifs',
+            aliases: [
+                'anime-gif',
+                'anime-gifs',
+            ],
+            memberName: 'animegif',
+            description: 'Provide a name of an anime show or character and I will return a gif!',
+            examples: [
+                '`' + config.prefix + 'animegif`',
+                '`' + config.prefix + 'anime-gif`',
+                '`' + config.prefix + 'anime-gifs`'
+                      ],
+            throttling: {
+                usages: 1,
+                duration: 4
+            }
+        });
+    }
 
-  run(message) {
-    fetch(`https://api.tenor.com/v1/random?key=${tenorAPI}&q=anime&limit=1`)
-      .then(res => res.json())
-      .then(json => message.channel.send(json.results[0].url))
-      .catch(function onError() {
-        message.reply(':x: Failed to find a gif!');
-        return;
-      });
-  }
+    run(message) {
+        const embed = new MessageEmbed();    
+        fetch(
+            'https://api.tenor.com/v1/random?key=' + config.tenorAPI + '&q=anime&limit=1'
+        )
+        .then(res => res.json())
+        .then(json => {
+            embed.setColor("RANDOM")
+            embed.setImage(json.results[0].media[0].gif.url);
+            message.channel.send(embed)
+        })
+            .catch(e => {
+                message.channel.send(':x: Failed to fetch a gif')
+            .console.error(e);
+            return;
+        })
+    };
 };
