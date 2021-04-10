9f5e2b1251a700d1328be677ac07eeb3069ff303
Nimbi <Darkwater409@gmail.com>
2021-04-10 02:50:25 -0700

    Update gif.js
    **Changelog**:
     - Updated response to RichEmbed to make it look cleaner
     - Updated error log

--- commands/gifs/gif.js
+++ commands/gifs/gif.js
@@ -1,43 +1,51 @@
 const fetch = require('node-fetch');
-const { tenorAPI } = require('../../config.json');
+const config = require('../../config.json');
 const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
 
-// Skips loading if not found in config.json
-if (!tenorAPI) return;
+if (!config.tenorAPI) return;
 
 module.exports = class GifCommand extends Command {
-  constructor(client) {
-    super(client, {
-      name: 'gif',
-      group: 'gifs',
-      aliases: ['search-gif', 'search-gifs'],
-      memberName: 'gif',
-      description: 'Provide a query and replies with a gif!',
-      throttling: {
-        usages: 1,
-        duration: 4
-      },
-      args: [
-        {
-          key: 'text',
-          prompt: ':thinking: What gif would you like to watch?',
-          type: 'string',
-          validate: function validateText(text) {
-            return text.length < 50;
-          }
-        }
-      ]
-    });
-  }
+    constructor(client) {
+        super(client, {
+            name: 'gif',
+            group: 'gifs',
+            aliases: ['search-gif', 'search-gifs'],
+            memberName: 'gif',
+            description: 'Provide a query and replies with a gif!',
+            examples: [
+                '`' + config.prefix + 'gif puppy`',
+                      ],
+            throttling: {
+                usages: 1,
+                duration: 4
+            },
+            args: [{
+                key: 'text',
+                prompt: 'What gif would you like to post?',
+                type: 'string',
+                validate: function validateText(text) {
+                    return text.length < 50;
+                }
+            }]
+        });
+    }
 
-  run(message, { text }) {
-    fetch(`https://api.tenor.com/v1/random?key=${tenorAPI}&q=${text}&limit=1`)
-      .then(res => res.json())
-      .then(json => message.channel.send(json.results[0].url))
-      .catch(function onError() {
-        message.reply(':x: Failed to find a gif that matched your query!');
-        // console.error(e);
-        return;
-      });
-  }
+    run(message, { text }) {
+        const embed = new MessageEmbed();
+        fetch(
+            'https://api.tenor.com/v1/random?key=' + config.tenorAPI + '&q=' + text + '&limit=1'
+        )
+            .then(res => res.json())
+            .then(json => {
+            embed.setColor("RANDOM")
+            embed.setImage(json.results[0].media[0].gif.url);
+            message.channel.send(embed)
+        })
+            .catch(e => {
+                message.channel.send('Failed to fetch a gif')
+            .console.error(e);
+            return;
+        })
+    };
 };
