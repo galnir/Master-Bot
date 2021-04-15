434fb7c247d93f833f0a4b4a1a3df277b79126f7
Nir <nirnir122@gmail.com>
2021-04-16 00:26:27 +0300

    fix the api base URL and make the command return a random gif

--- commands/gifs/animegif.js
+++ commands/gifs/animegif.js
@@ -12,8 +12,7 @@ module.exports = class AnimegifCommand extends Command {
       group: 'gifs',
       aliases: ['anime-gif', 'anime-gifs'],
       memberName: 'animegif',
-      description:
-        'Provide a name of an anime show or character and I will return a gif!',
+      description: 'Provides a random anime gif',
       throttling: {
         usages: 1,
         duration: 4
@@ -22,9 +21,11 @@ module.exports = class AnimegifCommand extends Command {
   }
 
   run(message) {
-    fetch(`https://api.tenor.com/v1/random?key=${tenorAPI}&q=anime&limit=1`)
+    fetch(`https://g.tenor.com/v1/random?key=${tenorAPI}&q=anime&limit=50`)
       .then(res => res.json())
-      .then(json => message.channel.send(json.results[0].url))
+      .then(json =>
+        message.channel.send(json.results[Math.floor(Math.random() * 49)].url)
+      )
       .catch(function onError() {
         message.reply(':x: Failed to find a gif!');
         return;
