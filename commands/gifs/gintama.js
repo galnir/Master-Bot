452df1b3c56a75f66e0508de5fb3ada71a4ae7de
Nir <nirnir122@gmail.com>
2021-04-16 00:32:31 +0300

    fix tenor api base URL

--- commands/gifs/gintama.js
+++ commands/gifs/gintama.js
@@ -39,7 +39,7 @@ module.exports = class GintamaCommand extends Command {
     
       
        fetch(
-        `https://api.tenor.com/v1/random?key=${tenorAPI}&q=gintama&limit=1`
+        `https://g.tenor.com/v1/random?key=${tenorAPI}&q=gintama&limit=1`
       )
         .then(res => res.json())
         .then(json => message.channel.send(json.results[0].url))
