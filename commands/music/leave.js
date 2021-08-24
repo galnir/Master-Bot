7ba49cdca2cc436a17c613622a09d058787751d2
Nir <nirnir122@gmail.com>
2021-08-24 16:15:33 +0300

    rewrite leave command using v13

---
+++ commands/music/leave.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { AudioPlayerStatus } = require('@discordjs/voice');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('leave')
+    .setDescription('Leaves a voice channel if in one!'),
+  async execute(interaction) {
+    const voiceChannel = interaction.member.voice.channel;
+    if (!voiceChannel) {
+      return interaction.reply('Please join a voice channel and try again!');
+    }
+
+    const player = interaction.client.playerManager.get(interaction.guildId);
+    if (
+      player.audioPlayer.state.status !== AudioPlayerStatus.Playing ||
+      !player
+    ) {
+      return interaction.reply('There is no song playing right now!');
+    } else if (voiceChannel.id !== interaction.guild.me.voice.channel.id) {
+      return interaction.reply(
+        'You must be in the same voice channel as the bot in order to skip!'
+      );
+    }
+
+    player.connection.destroy();
+    interaction.client.playerManager.delete(interaction.guildId);
+    return interaction.reply('Left your voice channel!');
+  }
+};
