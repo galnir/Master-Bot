2399e931e38128c5501b258251cf18b0c5ecfd45
Nir <nirnir122@gmail.com>
2021-08-25 13:19:16 +0300

    remove music data from guildData object

--- utils/createGuildData.js
+++ utils/createGuildData.js
@@ -1,17 +1,5 @@
 module.exports = function() {
   return {
-    musicData: {
-      queue: [],
-      queueHistory: [],
-      isPlaying: false,
-      isPreviousTrack: false,
-      nowPlaying: null,
-      songDispatcher: null,
-      skipTimer: false, // only skip if user used leave command
-      loopSong: false,
-      loopQueue: false,
-      volume: 1
-    },
     triviaData: {
       isTriviaRunning: false,
       wasTriviaEndCalled: false,
