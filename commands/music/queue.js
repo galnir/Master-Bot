f50ee530b61632545b4c929144fc18df6147fc20
Nir <nirnir122@gmail.com>
2021-08-28 01:39:26 +0300

    add buttons to queue embed

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -1,12 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageEmbed } = require('discord.js');
+const { PagesBuilder } = require('discord.js-pages');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('queue')
     .setDescription('Display the music queue'),
   execute(interaction) {
-    interaction.deferReply();
+    //interaction.deferReply({ ephemeral: false });
     const guildData = interaction.client.guildData.get(interaction.guildId);
     if (guildData) {
       if (guildData.triviaData.isTriviaRunning) {
@@ -24,19 +25,29 @@ module.exports = {
       return interaction.followUp(':x: There is nothing playing right now!');
     }
 
-    const queueClone = player.queue.slice(0, 24);
-    const queueEmbed = new MessageEmbed()
-      .setColor('#CCE763')
-      .setTitle('Music Queue')
-      .setTimestamp();
-    const fields = [];
-    for (let i = 0; i < queueClone.length; i++) {
-      fields.push({
-        name: `${i + 1}`,
-        value: `${queueClone[i].title}`
-      });
+    const queueClone = player.queue;
+    const embeds = [];
+
+    for (let i = 0; i < Math.ceil(queueClone.length / 24); i++) {
+      const playlistArray = queueClone.splice(0, 24);
+      const fields = [];
+      for (let j = 0; j < playlistArray.length; j++) {
+        fields.push({
+          name: `${j + 1}`,
+          value: `${playlistArray[j].title}`
+        });
+      }
+      embeds.push(new MessageEmbed().setTitle(`Page ${i}`).setFields(fields));
     }
-    queueEmbed.setFields(fields);
-    interaction.followUp({ embeds: [queueEmbed] });
+
+    new PagesBuilder(interaction)
+      .setTitle('Music Queue')
+      .setPages(embeds)
+      .setColor('#9096e6')
+      .setAuthor(
+        interaction.member.user.username,
+        interaction.member.user.displayAvatarURL()
+      )
+      .build();
   }
 };
