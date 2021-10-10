cb7fe88aaa7197c204d8baa0e9a98c938f4a1ed1
Nir <36937694+galnir@users.noreply.github.com>
2021-10-10 17:26:17 +0300

    format Reminder.js

--- utils/models/Reminder.js
+++ utils/models/Reminder.js
@@ -1,31 +1,30 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const ReminderSchema = Schema({
-    text: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    },
-    guildId: {
-        type: Number,
-        required: true   
-    },
-    channelId: {
-        type: Number,
-        required: true   
-    },
-    time: {
-        type: Number,
-        required: true   
-    },
-    createdAt: {
-        type: Number,
-        default: Date.now()
-    }
-
+  text: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  guildId: {
+    type: Number,
+    required: true
+  },
+  channelId: {
+    type: Number,
+    required: true
+  },
+  time: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now()
+  }
 });
 
-module.exports = model("Reminder", ReminderSchema);
+module.exports = model('Reminder', ReminderSchema);
