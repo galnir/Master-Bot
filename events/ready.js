ab3a9f144f8eb38ca77b76d55178a8b3357b1c1d
Nir <36937694+galnir@users.noreply.github.com>
2021-10-10 17:26:40 +0300

    format ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,29 +5,26 @@ const Reminder = require('./utils/models/Reminder');
 const setUpReminders = require('./utils/setUpReminders');
 
 module.exports = {
-    name: 'ready',
-    once: true,
-    async execute(client) {
+  name: 'ready',
+  once: true,
+  async execute(client) {
+    client.playerManager = new Map();
+    client.triviaManager = new Map();
+    client.guildData = new Collection();
+    client.user.setActivity('/', { type: 'WATCHING' });
+    mongoose
+      .connect(encodeURI(mongo_URI), {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+      .then(() => {
+        console.log('Mongo is ready');
+      })
+      .catch(console.error);
 
-        client.playerManager = new Map();
-        client.triviaManager = new Map();
-        client.guildData = new Collection();
-        client.user.setActivity('/', { type: 'WATCHING' });
-        mongoose
-          .connect(encodeURI(mongo_URI), {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-          })
-          .then(() => {
-            console.log('Mongo is ready');
-          })
-          .catch(console.error);
+    const reminders = await Reminder.find({});
+    setUpReminders(reminders, client);
 
-          const reminders = await Reminder.find({});
-          setUpReminders(reminders, client);
-      
-        console.log('Ready!');
-
-    }
-  };
-  
+    console.log('Ready!');
+  }
+};
