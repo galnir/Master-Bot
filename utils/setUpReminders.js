65e0e6c1951302def157a1504045ed5a8a17b950
Nir <36937694+galnir@users.noreply.github.com>
2021-10-10 17:25:51 +0300

    format setUpReminders.js

--- utils/setUpReminders.js
+++ utils/setUpReminders.js
@@ -1,31 +1,26 @@
-const Reminder = require("./models/Reminder");
+const Reminder = require('./models/Reminder');
 
 const setUpReminders = (reminders, client) => {
+  if (reminders.length == 0) return;
 
-    if (reminders.length == 0) return;
+  reminders.forEach(remind => {
+    const time = remind.createdAt + remind.time * 60000 - new Date();
 
-    reminders.forEach((remind) => {
+    if (time > 0) {
+      //In case for some reason it was not deleted from the database.
+      setTimeout(async () => {
+        client.channels.cache.find(channel => {
+          if (channel.id == remind.channelId) {
+            channel.send(
+              `<@${remind.author}> :alarm_clock: Reminder: ${remind.text}`
+            );
 
-        const time = (remind.createdAt + remind.time * 60000) - new Date();
-
-        if ( time > 0 ) { //In case for some reason it was not deleted from the database.
-            setTimeout(async () => {
-
-                client.channels.cache.find((channel) => {
-                    
-                    if (channel.id == remind.channelId) {
-                        channel.send(`<@${remind.author}> :alarm_clock: Reminder: ${remind.text}`);
-                    
-                        Reminder.deleteOne({ _id: remind._id });
-                    
-                    }
-                })
-    
-            }, time)
-        }
-        
-    });
-
-}
+            Reminder.deleteOne({ _id: remind._id });
+          }
+        });
+      }, time);
+    }
+  });
+};
 
 module.exports = setUpReminders;
