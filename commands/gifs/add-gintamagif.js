368a753ddcd4d0130320800793c34cf07d81975d
Nimbi <Darkwater409@gmail.com>
2021-10-30 08:18:15 -0700

    Update add-gintamagif.js

--- commands/gifs/add-gintamagif.js
+++ commands/gifs/add-gintamagif.js
@@ -1,17 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { ownerID, devID } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('add-gintamagif')
-        .setDescription('Add a Gintama Gif.')
+        .setDescription('Add a Ginatma Gif.')
         .addStringOption(option =>
             option
                 .setName('gif')
                 .setDescription('What gif would you like to add?')
                 .setRequired(true)
         ),
-    execute(interaction) {
+    execute(interaction, message) {
         const gif = interaction.options.get('gif').value;
-        fs.writeTxtFILE('././resources/gifs/gintamalinks.txt', gif + '\n', { flag: 'a+' });
+        if ( message.mentions.users.id === ownerID || devID )
+            return fs.writeTxtFILE('././resources/gifs/gintamaliks.txt', gif + '\n', { flag: 'a+' })
+        else if not ( message.mentions.users.id === ownerID || devID )
+            return message.reply(":x: You do not have permission to use this command");
     }
 };
