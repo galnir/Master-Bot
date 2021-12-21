9119a0968ec65661cbf5a0e093691d7d5544f1df
galnir <nirnir122@gmail.com>
2021-12-21 13:37:01 +0200

    rewrite shuffle command using lavalink

--- commands/music/shuffle.js
+++ commands/music/shuffle.js
@@ -3,48 +3,21 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('shuffle')
-    .setDescription('Shuffle the music queue!'),
-
+    .setDescription('Shuffle the music queue'),
   execute(interaction) {
-    interaction.deferReply();
-    const voiceChannel = interaction.member.voice.channel;
-    if (!voiceChannel) {
-      return interaction.followUp(
-        `:no_entry: You must be in the same voice channel as the bot in order to use that!`
-      );
-    } else if (voiceChannel.id !== interaction.guild.me.voice.channel.id) {
-      return interaction.followUp(
-        `:no_entry: You must be in the same voice channel as the bot in order to use that!`
-      );
-    }
-    const player = interaction.client.playerManager.get(interaction.guildId);
+    const client = interaction.client;
+    const player = client.music.players.get(interaction.guild.id);
     if (!player) {
-      return interaction.followUp(':x: There is nothing playing right now!');
-    } else if (player.loopSong) {
-      return interaction.followUp(
-        ':x: Turn off the **loop** command before using the **shuffle** command!'
-      );
+      return interaction.reply('There is nothing playing at the moment!');
     }
 
-    if (player.queue.length < 1) {
-      return interaction.followUp('There are no songs in queue!');
+    const voiceChannel = interaction.member.voice.channel;
+    if (!voiceChannel || voiceChannel.id !== player.channelId) {
+      return interaction.reply('Join my voice channel and try again!');
     }
 
-    if (player.commandLock) {
-      return interaction.followUp(
-        'Please wait until play command is done processing'
-      );
-    }
+    player.queue.shuffle();
 
-    shuffleQueue(player.queue);
-
-    return interaction.followUp('The music queue has been shuffled!');
+    interaction.reply('Queue shuffled');
   }
 };
-
-function shuffleQueue(queue) {
-  for (let i = queue.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [queue[i], queue[j]] = [queue[j], queue[i]];
-  }
-}
