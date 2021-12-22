7365a0afe1a235deca8ad17c904f27f6136def22
galnir <nirnir122@gmail.com>
2021-12-22 21:00:11 +0200

    rewrite remove using lavalink

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -3,39 +3,34 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('remove')
-    .setDescription('Remove a specific song from queue')
-    .addIntegerOption(option =>
-      option
+    .setDescription('Remove a song from the queue')
+    .addIntegerOption((option) => {
+      return option
         .setName('position')
-        .setDescription('What song number do you want to remove from queue?')
-        .setRequired(true)
-    ),
+        .setDescription(
+          'What is the position of the song you want to remove from the queue?'
+        )
+        .setRequired(true);
+    }),
   execute(interaction) {
-    const position = interaction.options.get('position').value;
-    const player = interaction.client.playerManager.get(interaction.guildId);
+    const client = interaction.client;
+    const player = client.music.players.get(interaction.guildId);
 
     if (!player) {
-      return interaction.reply('There is nothing playing now!');
+      return interaction.reply('There is nothing playing at the moment!');
     }
 
     const voiceChannel = interaction.member.voice.channel;
-
-    if (!voiceChannel) {
-      return interaction.reply(
-        ':no_entry: Please join a voice channel and try again!'
-      );
-    } else if (voiceChannel.id !== interaction.guild.me.voice.channel.id) {
-      interaction.reply(
-        `:no_entry: You must be in the same voice channel as the bot in order to use that!`
-      );
-      return;
+    if (voiceChannel.id !== player.channelId) {
+      return interaction.reply('Join my voice channel and try again!');
     }
 
-    if (position < 1 || position > player.queue.length) {
-      return interaction.reply('Please enter a valid position!');
+    const position = interaction.options.get('position').value;
+    if (position < 1 || position > player.queue.tracks.length) {
+      return interaction.reply('Please enter a valid position number!');
     }
 
-    player.queue.splice(position - 1, 1);
+    player.queue.tracks.splice(position - 1, 1);
     return interaction.reply(
       `:wastebasket: Removed song number ${position} from queue!`
     );
