ebb046ce8c403306c0049006d12b8e8024c7b359
galnir <nirnir122@gmail.com>
2021-12-25 23:50:52 +0200

    add nightcore filter command

---
+++ commands/music/nightcore.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('nightcore')
+    .setDescription('Enable/Disable Nightcore filter'),
+  async execute(interaction) {
+    const client = interaction.client;
+    const player = client.music.players.get(interaction.guildId);
+
+    if (!player) {
+      return interaction.reply('There is nothing playing at the moment!');
+    }
+
+    const voiceChannel = interaction.member.voice.channel;
+    if (voiceChannel.id !== player.channelId) {
+      return interaction.reply(
+        'You must be in my channel in order to use that!'
+      );
+    }
+
+    player.filters.timescale = (player.nightcore = !player.nightcore)
+      ? { speed: 1.125, pitch: 1.125, rate: 1 }
+      : undefined;
+
+    await player.setFilters();
+    return interaction.reply(
+      `Nightcore ${player.nightcore ? 'enabled' : 'disabled'}`
+    );
+  }
+};
