4eaacfbf886a27f05ddb1b929edc70db202c140b
galnir <nirnir122@gmail.com>
2021-12-26 12:54:05 +0200

    add karaoke filter command

---
+++ commands/music/karaoke.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('karaoke')
+    .setDescription('Turn the playing track to karaoke!'),
+  async execute(interaction) {
+    const client = interaction.client;
+    const player = client.music.players.get(interaction.guildId);
+
+    if (!player) {
+      return interaction.reply('There is nothing playing at the moment!');
+    }
+
+    const voiceChannel = interaction.member.voice.channel;
+    if (voiceChannel.id !== player.channelId) {
+      return interaction.reply('Please join my voice channel and try again!');
+    }
+
+    player.filters.karaoke = (player.karaoke = !player.karaoke)
+      ? {
+          level: 1,
+          monoLevel: 1,
+          filterBand: 220,
+          filterWidth: 100
+        }
+      : undefined;
+
+    await player.setFilters();
+    return interaction.reply(
+      `Karaoke ${player.bassboost ? 'enabled' : 'disabled'}`
+    );
+  }
+};
