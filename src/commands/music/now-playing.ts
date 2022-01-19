c9919b998105a03b8e16d070b282bc084b6ac4f0
galnir <nirnir122@gmail.com>
2022-01-19 10:19:35 +0200

    rewrite nowplaying command with TypeScript and Sapphire

---
+++ src/commands/music/now-playing.ts
@@ -0,0 +1,40 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import {
+  ApplicationCommandRegistry,
+  Command,
+  CommandOptions
+} from '@sapphire/framework';
+import type { CommandInteraction } from 'discord.js';
+import { container } from '@sapphire/framework';
+import { NowPlayingEmbed } from '../../lib/music/NowPlayingEmbed';
+import type { Song } from '@lavaclient/queue';
+
+@ApplyOptions<CommandOptions>({
+  name: 'now-playing',
+  description: 'Display an embed detailing the song playing',
+  preconditions: ['inVoiceChannel', 'playerIsPlaying', 'inPlayerVoiceChannel']
+})
+export class NowPlayingCommand extends Command {
+  public override async chatInputRun(interaction: CommandInteraction) {
+    const { client } = container;
+
+    const player = client.music.players.get(interaction.guild!.id);
+
+    const NowPlaying = new NowPlayingEmbed(
+      player?.queue.current as Song,
+      player?.accuratePosition,
+      player?.queue.current!.length as number
+    );
+
+    return await interaction.reply({ embeds: [NowPlaying.NowPlayingEmbed()] });
+  }
+
+  public override registerApplicationCommands(
+    registery: ApplicationCommandRegistry
+  ): void {
+    registery.registerChatInputCommand({
+      name: this.name,
+      description: this.description
+    });
+  }
+}
