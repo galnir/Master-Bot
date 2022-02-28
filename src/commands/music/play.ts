89a4fc9e7191819079d69550c923fad2c4626299
galnir <nirnir122@gmail.com>
2022-02-28 21:35:11 +0200

    integrate saved playlists feature into play command

--- src/commands/music/play.ts
+++ src/commands/music/play.ts
@@ -4,10 +4,14 @@ import {
   Command,
   CommandOptions
 } from '@sapphire/framework';
-import type { CommandInteraction } from 'discord.js';
+import type { CommandInteraction, GuildMember } from 'discord.js';
 import { container } from '@sapphire/framework';
 import type { MessageChannel } from '../..';
 import searchSong from '../../lib/utils/music/searchSong';
+import Member from '../../lib/models/Member';
+import type { SavedPlaylist } from './create-playlist';
+import type { Addable } from '../../lib/utils/queue/Queue';
+//import Member from '../../lib/models/Member';
 
 @ApplyOptions<CommandOptions>({
   name: 'play',
@@ -15,23 +19,60 @@ import searchSong from '../../lib/utils/music/searchSong';
   preconditions: [
     'inVoiceChannel',
     'musicTriviaPlaying',
-    'inPlayerVoiceChannel'
+    'inPlayerVoiceChannel',
+    'userInDB'
   ]
 })
 export class PlayCommand extends Command {
   public override async chatInputRun(interaction: CommandInteraction) {
     await interaction.deferReply();
+    //const interactionMember = interaction.member as GuildMember;
     const { client } = container;
     const query = interaction.options.getString('query', true);
+    const isCustomPlaylist =
+      interaction.options.getString('is-custom-playlist');
+
+    const interactionMember = interaction.member as GuildMember;
 
     // had a precondition make sure the user is infact in a voice channel
     const voiceChannel = interaction.guild?.voiceStates?.cache?.get(
       interaction.user.id
     )?.channel;
 
-    const trackTuple = await searchSong(query);
-    if (!trackTuple[1].length) {
-      return await interaction.followUp({ content: trackTuple[0] as string });
+    let tracks: Addable[] = [];
+    let message: string = '';
+    if (isCustomPlaylist == 'Yes') {
+      const member = await Member.findOne({
+        memberId: interactionMember.id
+      });
+
+      const savedPlaylists: SavedPlaylist[] = member.savedPlaylists;
+
+      const index = savedPlaylists.findIndex(element => element.name === query);
+      if (index !== -1) {
+        const urls = savedPlaylists[index].urls;
+
+        for (let i = 0; i < urls.length; i++) {
+          const track = await searchSong(urls[i].url as string);
+          if (!track[1].length) continue;
+
+          tracks.push(...track[1]);
+        }
+
+        message = `Added tracks from **${query}** to the queue!`;
+      } else {
+        return await interaction.followUp({
+          content: `You have no custom playlist named **${query}**!`,
+          ephemeral: true
+        });
+      }
+    } else {
+      const trackTuple = await searchSong(query);
+      if (!trackTuple[1].length) {
+        return await interaction.followUp({ content: trackTuple[0] as string }); // error
+      }
+      message = trackTuple[0];
+      tracks.push(...trackTuple[1]);
     }
 
     let player = client.music.players.get(interaction.guild!.id);
@@ -44,9 +85,8 @@ export class PlayCommand extends Command {
 
     const started = player.playing || player.paused;
 
-    await interaction.followUp({ content: trackTuple[0] as string });
-
-    player.queue.add(trackTuple[1], { requester: interaction.user.id });
+    await interaction.followUp({ content: message });
+    player.queue.add(tracks, { requester: interaction.user.id });
     if (!started) {
       await player.queue.start();
     }
@@ -65,6 +105,21 @@ export class PlayCommand extends Command {
           description: 'What song or playlist would you like to listen to?',
           type: 'STRING',
           required: true
+        },
+        {
+          name: 'is-custom-playlist',
+          description: 'Is it a custom playlist?',
+          type: 'STRING',
+          choices: [
+            {
+              name: 'Yes',
+              value: 'Yes'
+            },
+            {
+              name: 'No',
+              value: 'No'
+            }
+          ]
         }
       ]
     });
