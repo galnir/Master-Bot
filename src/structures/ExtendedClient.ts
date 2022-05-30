d073a17fb5297801890b1439f676b4657081f9a1
Bacon-Fixation <baconfixation@gmail.com>
2022-05-29 23:38:05 -0500

    Adds Missing Intents for Reactions

--- src/structures/ExtendedClient.ts
+++ src/structures/ExtendedClient.ts
@@ -22,6 +22,7 @@ export class ExtendedClient extends SapphireClient {
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_VOICE_STATES
       ]
     });
