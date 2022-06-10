0b10bf422d49586bf148a345b9901b25db1f5bb3
Bacon-Fixation <baconfixation@gmail.com>
2022-06-10 06:54:21 -0500

    adapting repo changes

--- src/structures/ExtendedClient.ts
+++ src/structures/ExtendedClient.ts
@@ -1,17 +1,13 @@
-import type { ClientTwitchExtension } from './../lib/utils/twitch/twitchAPI-types';
+import type { ClientTwitchExtension } from '../lib/utils/twitch/twitchAPI-types';
+import { TwitchAPI } from '../lib/utils/twitch/twitchAPI';
 import { SapphireClient } from '@sapphire/framework';
 import { Intents, User } from 'discord.js';
-import { Node } from 'lavaclient';
+import { QueueClient } from '../lib/utils/queue/QueueClient';
 import * as data from '../config.json';
-import { embedButtons } from '../lib/utils/music/ButtonHandler';
-import { NowPlayingEmbed } from './../lib/utils/music/NowPlayingEmbed';
-import { manageStageChannel } from './../lib/utils/music/channelHandler';
-import { TwitchAPI } from '../lib/utils/twitch/twitchAPI';
-import { inactivityTime } from '../lib/utils/music/handleOptions';
+import Redis from 'ioredis';
 
 export class ExtendedClient extends SapphireClient {
-  readonly music: Node;
-  playerEmbeds: { [key: string]: string };
+  readonly music: QueueClient;
   leaveTimers: { [key: string]: NodeJS.Timer };
   twitch: ClientTwitchExtension = {
     api: new TwitchAPI(data.twitchClientID, data.twitchClientSecret),
@@ -35,7 +31,6 @@ export class ExtendedClient extends SapphireClient {
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_VOICE_STATES
       ]
     });
@@ -45,9 +40,10 @@ export class ExtendedClient extends SapphireClient {
       tictactoePlayers: new Map()
     };
 
-    this.music = new Node({
+    this.music = new QueueClient({
       sendGatewayPayload: (id, payload) =>
         this.guilds.cache.get(id)?.shard?.send(payload),
+      options: { redis: new Redis() },
       connection: {
         host: data.lava_host,
         password: data.lava_pass,
@@ -55,6 +51,18 @@ export class ExtendedClient extends SapphireClient {
         secure: data.lava_secure
       }
     });
+    this.ws.on('VOICE_SERVER_UPDATE', async data => {
+      // handle if a mod right-clicks disconnect on the bot
+      if (!data.channel_id && data.user_id === this.application?.id) {
+        const queue = this.music.queues.get(data.guild_id!);
+        await queue.leave();
+      }
+      this.music.handleVoiceUpdate(data);
+    });
+    this.ws.on('VOICE_STATE_UPDATE', data => {
+      this.music.handleVoiceUpdate(data);
+    });
+
     if (data.twitchClientID && data.twitchClientSecret) {
       this.twitch.api?.getAccessToken('user:read:email').then(response => {
         this.twitch.auth = {
@@ -86,62 +94,16 @@ export class ExtendedClient extends SapphireClient {
       console.log('Twitch-Features are Disabled');
     }
 
-    this.ws.on('VOICE_SERVER_UPDATE', data => {
-      this.music.handleVoiceUpdate(data);
-    });
-    this.ws.on('VOICE_STATE_UPDATE', data => {
-      this.music.handleVoiceUpdate(data);
-    });
-
-    this.playerEmbeds = {};
     this.leaveTimers = {};
-    this.music.on('queueFinish', queue => {
-      queue.player.stop();
-
-      this.leaveTimers[queue.player.guildId] = setTimeout(() => {
-        queue.channel!.send(':zzz: Leaving due to inactivity');
-        queue.player.disconnect();
-        queue.player.node.destroyPlayer(queue.player.guildId);
-      }, inactivityTime());
-      delete this.playerEmbeds[queue.player.guildId];
-    });
-
-    this.music.on('trackStart', async (queue, song) => {
-      if (this.leaveTimers[queue.player.guildId]) {
-        clearTimeout(this.leaveTimers[queue.player.guildId]!);
-      }
-
-      const NowPlaying = new NowPlayingEmbed(
-        song,
-        0,
-        queue.current!.length as number,
-        queue.player.volume,
-        queue.tracks!,
-        queue.last!,
-        false
-      );
-
-      await embedButtons(NowPlaying.NowPlayingEmbed(), queue, song);
-
-      const voiceChannel = this.voice.client.channels.cache.get(
-        queue.player.channelId!
-      );
-
-      // Stage Channels
-      if (voiceChannel?.type === 'GUILD_STAGE_VOICE') {
-        const botUser = voiceChannel?.members.get(this.application?.id!);
-        await manageStageChannel(voiceChannel, botUser!, queue);
-      }
-    });
   }
 }
 
 declare module '@sapphire/framework' {
   interface SapphireClient {
-    readonly music: Node;
-    playerEmbeds: { [key: string]: string };
+    readonly music: QueueClient;
     leaveTimers: { [key: string]: NodeJS.Timer };
     twitch: ClientTwitchExtension;
+
     gameData: {
       connect4Players: Map<string, User>;
       tictactoePlayers: Map<string, User>;
