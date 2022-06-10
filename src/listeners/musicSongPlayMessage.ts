d0f1929a0629d4c143a64d19eb5eed4b5d129c8c
Bacon-Fixation <baconfixation@gmail.com>
2022-06-10 06:43:36 -0500

    Stage Channel Support/Leave on Empty Voice Channel

--- src/listeners/musicSongPlayMessage.ts
+++ src/listeners/musicSongPlayMessage.ts
@@ -1,6 +1,7 @@
 import { ApplyOptions } from '@sapphire/decorators';
-import { Listener, ListenerOptions } from '@sapphire/framework';
+import { container, Listener, ListenerOptions } from '@sapphire/framework';
 import type { TextChannel } from 'discord.js';
+import { manageStageChannel } from '../lib/utils/music/channelHandler';
 import type { Song } from '../lib/utils/queue/Song';
 
 @ApplyOptions<ListenerOptions>({
@@ -8,6 +9,19 @@ import type { Song } from '../lib/utils/queue/Song';
 })
 export class MusicSongPlayMessageListener extends Listener {
   public override async run(channel: TextChannel, track: Song): Promise<void> {
+    const { client } = container;
+    const queue = client.music.queues.get(channel.guild.id);
+
+    await manageStageChannel(
+      channel.guild.me?.voice.channel!,
+      channel.guild.me!,
+      queue
+    );
+    // Leave Voice Channel when attempting to stream to an empty channel
+    if (channel.guild.me?.voice.channel?.members.size == 1) {
+      await queue.leave();
+      return;
+    }
     await channel.send({ content: `Now playing: ${track.title}` });
   }
 }
