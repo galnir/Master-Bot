6a6905c70f0474cc88d3f1e8481512eeb5bc3ea1
galnir <nirnir122@gmail.com>
2022-06-20 00:46:30 +0300

    toggle button styling

--- dashboard/app/routes/dashboard/$id/welcome.tsx
+++ dashboard/app/routes/dashboard/$id/welcome.tsx
@@ -93,10 +93,23 @@ export default function WelcomeScreen() {
             <button
               role="checkbox"
               onClick={() => setWelcomeMessageEdit(!welcomeMessageEdit)}
-              className="h-7 w-14 outline-none bg-green-400"
               aria-checked={welcome_message_enabled ? "true" : "false"}
               aria-label={welcome_message_enabled ? "checked" : "unchecked"}
-            />
+            >
+              <label
+                htmlFor="default-toggle"
+                className="inline-flex relative items-center cursor-pointer"
+              >
+                <input
+                  type="checkbox"
+                  value=""
+                  id="default-toggle"
+                  checked={welcomeMessageEdit}
+                  className="sr-only peer"
+                />
+                <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+              </label>
+            </button>
           </fetcher.Form>
         </div>
       </div>
