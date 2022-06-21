dff76c1ed38643c87219a4f14e8c77e282e301ca
Bacon-Fixation <baconfixation@gmail.com>
2022-06-21 14:36:26 -0500

    Reminder - Direct Message Intents

--- bot/src/structures/ExtendedClient.ts
+++ bot/src/structures/ExtendedClient.ts
@@ -29,6 +29,8 @@ export class ExtendedClient extends SapphireClient {
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
         Intents.FLAGS.GUILD_VOICE_STATES
       ],
       api: {
