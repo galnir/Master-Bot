780923e46ef47c0910780f1566bbe6205c20786b
galnir <nirnir122@gmail.com>
2022-07-03 16:15:10 +0300

    if user hits cancel on auth return to /

--- dashboard/app/routes/auth/$provider.callback.tsx
+++ dashboard/app/routes/auth/$provider.callback.tsx
@@ -1,8 +1,16 @@
-import type { LoaderFunction } from "@remix-run/node";
+import { type LoaderFunction, redirect } from "@remix-run/node";
 import { authenticator } from "~/server/auth.server";
 
 export let loader: LoaderFunction = async ({ request, params }) => {
   if (!params.provider) return;
+
+  const url = new URL(request.url);
+  const error = url.searchParams.get("error");
+
+  if (error) {
+    return redirect("/");
+  }
+
   return await authenticator.authenticate("discord", request, {
     successRedirect: "/dashboard",
     failureRedirect: "/login",
