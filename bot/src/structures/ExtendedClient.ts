3c18b9b9e2ecb35a294fa71cd22f45f8a2986af1
Bacon-Fixation <baconfixation@gmail.com>
2022-08-12 15:59:08 -0500

    Update ExtendedClient.ts

--- bot/src/structures/ExtendedClient.ts
+++ bot/src/structures/ExtendedClient.ts
@@ -57,9 +57,7 @@ export class ExtendedClient extends SapphireClient {
         redis: new Redis({
           host: process.env.REDIS_HOST || 'localhost',
           port: Number.parseInt(process.env.REDIS_PORT!) || 6379,
-
           password: process.env.REDIS_PASSWORD || '',
-
           db: Number.parseInt(process.env.REDIS_DB!) || 0
         })
       },
