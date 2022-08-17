1085f16c8928036118695158acaaa5e3d96715f9
galnir <nirnir122@gmail.com>
2022-08-17 12:54:07 +0300

    import files from deleted branch

---
+++ dashboard/src/components/GuildSelectBox/index.ts
@@ -0,0 +1,2 @@
+export { default } from "./GuildSelectBox";
+export * from "./GuildSelectBox";
---
+++ dashboard/src/components/HeaderButton/HeaderButton.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+
+interface HeaderButtonProps {
+  children: React.ReactNode;
+  linkTo: string;
+  newWindow?: boolean;
+  external?: boolean;
+}
+
+const HeaderButton = ({
+  children,
+  linkTo,
+  newWindow,
+  external,
+}: HeaderButtonProps) => {
+  const linkStyles =
+    "p-3 text-white bg-purple-600 rounded font-light hover:bg-purple-500";
+  return (
+    <div>
+      {external ? (
+        <a
+          href={linkTo}
+          className={linkStyles}
+          target={newWindow ? "_blank" : ""}
+          rel="noreferrer"
+        >
+          {children}
+        </a>
+      ) : (
+        <Link href={linkTo} target={newWindow ? "_blank" : ""}>
+          <a className={linkStyles}>{children}</a>
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default HeaderButton;
