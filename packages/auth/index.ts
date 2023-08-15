c3ad039afc864d104e0bae9f61a900a7910179b3
galnir <nirnir122@gmail.com>
2023-08-15 16:51:10 +0300

    attempt to refresh tokens on session ?

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -60,14 +60,62 @@ export const {
 		})
 	],
 	callbacks: {
-		session: ({ session, user }) => ({
-			...session,
-			user: {
-				...session.user,
-				id: user.id,
-				discordId: user.discordId
+		session: async ({ session, user }) => {
+			const account = await prisma.account.findUnique({
+				where: {
+					userId: user.id
+				}
+			});
+
+			if (account?.expires_at * 1000 < Date.now()) {
+				// refresh token
+				try {
+					const response = await fetch(
+						'https://discord.com/api/v10/oauth2/token',
+						{
+							headers: {
+								'Content-Type': 'application/x-www-form-urlencoded'
+							},
+							method: 'POST',
+							body: new URLSearchParams({
+								grant_type: 'refresh_token',
+								client_id: env.DISCORD_CLIENT_ID,
+								client_secret: env.DISCORD_CLIENT_SECRET,
+								refresh_token: account.refresh_token
+							})
+						}
+					);
+
+					if (!response.ok) {
+						throw new Error('Failed to refresh token');
+					}
+
+					const data = await response.json();
+
+					await prisma.account.update({
+						where: {
+							userId: user.id
+						},
+						data: {
+							access_token: data.access_token,
+							refresh_token: data.refresh_token,
+							expires_at: data.expires_in
+						}
+					});
+				} catch (error) {
+					console.log(error);
+				}
 			}
-		})
+
+			return {
+				...session,
+				user: {
+					...session.user,
+					id: user.id,
+					discordId: user.discordId
+				}
+			};
+		}
 
 		// @TODO - if you wanna have auth on the edge
 		// jwt: ({ token, profile }) => {
